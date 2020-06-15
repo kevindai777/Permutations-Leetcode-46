94e4a4e1f0e61371658a92e68722f96120916391
kevindai777 <kevin.dai.2451@gmail.com>
2020-06-14 22:50:19 -0400

    solutions

---
+++ permutations.js
@@ -0,0 +1,34 @@
+//Given a 1-D array of integers, give a 2-D array of all possible permutations
+//of the given array.
+
+let nums = [1,2,3]
+
+
+//O(O(∑ (k=1N) P(N,k)) where P(N, k) = {N!}/{(N - k)!} = N (N - 1) ... (N - k + 1)
+
+//Here we have a temp array to hold our different permutations from the given array
+//Once we exhaust the values from the given array, we push that permutation into the output array
+let temp = []
+let output = []
+
+function helper(temp, nums, output) {
+    if (nums.length == 0) {
+        output.push(temp.slice())
+        return
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        //push in the value into the temp array
+        temp.push(nums[i])
+
+        //remove the value from the given array
+        nums.splice(i, 1)
+        helper(temp, nums, output)
+
+        //Here, we pop in the next value of the given array to permute
+        nums.splice(i, 0, temp.pop())
+    }
+}
+helper(temp, nums, output)
+
+console.log(output)
